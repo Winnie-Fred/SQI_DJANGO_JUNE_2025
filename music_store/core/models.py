9cb374b50141a328ad96c9de5bbf3166cc40ebf5
Winifred Igboama <winiigboama@gmail.com>
2025-06-16 13:55:51 +0100

    teach built-in and custom validators

--- music_store/core/models.py
+++ music_store/core/models.py
@@ -1,16 +1,28 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.core.exceptions import ValidationError
+
+from django.utils import timezone
 
 # Create your models here.
 
+
+def validate_debut_year(year):
+    current_year = timezone.now().year
+    # if not (1 <= year <= current_year):
+    if year not in range(1, current_year+1):
+        raise ValidationError(f"Debut year must fall between 1 and {current_year}")
+
 class Artist(models.Model):
-    name = models.CharField(max_length=100)
-    debut_year = models.IntegerField()
+    name = models.CharField(max_length=100, validators=[MinLengthValidator(4)])
+    debut_year = models.IntegerField(validators=[validate_debut_year])
+    image = models.ImageField(upload_to='artist_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 class Album(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, validators=[MinLengthValidator(2)])  # min length of 2
     release_date = models.DateField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='albums')
 
